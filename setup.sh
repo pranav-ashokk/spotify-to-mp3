58ee490bf33b9ebcf89041fd73080b87e3ce2321
pranav-ashokk <pranavrashok@gmail.com>
2020-05-08 17:14:00 -0500

    Ready for client use

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt update
+sudo apt install ffmpeg
+sudo apt install npm
+sudo apt-get install java-common
+sudo apt-get install default-jdk
+npm install youtube-mp3-downloader
+
+exit
--- spotify_to_mp3.sh
+++ spotify_to_mp3.sh
@@ -5,14 +5,7 @@
 # Version: 1.0
 # spotify_to_mp3.sh - A script to locally download playlists or songs from Spotify as mp3 files
 
-# Install dependencies
-sudo apt update
-sudo apt install ffmpeg
-sudo apt install npm
-sudo apt-get install java-common
-sudo apt-get install default-jdk
-npm install youtube-mp3-downloader --save
-clear
+#clear
 RED='\033[0;31m'
 NC='\033[0m'
 
@@ -26,7 +19,7 @@ for arg in "$@"
         then
             printf "\n${RED}(HELP) Here is a list of commands you can use:${NC}"
             printf "\n--help | -h (For a list of commands)"
-            printf "\n--playlist playlistURL\n"
+            printf "\n--playlist playlistURL\n\n"
         fi
 
         # Playlist event
@@ -34,28 +27,34 @@ for arg in "$@"
         then
             INDEX=$((argNUM+1)) 
             eval url='$'$INDEX
+
+            # Catch 'no url' error
             if [ -z "$url" ]
             then
-                printf "\n${RED}Make sure to enter the playlist URL after '--playlist '!${NC}\n"
+                printf "\n${RED}Make sure to enter the playlist URL after '--playlist '!${NC}\n\n"
             fi
-            
+
             # Get YouTube links for playlist
             javac -cp "./lib/jsoup-1.13.1.jar" SpotifyScraper.java
             YT_URL_TEMP=$(java -cp "./lib/jsoup-1.13.1.jar" SpotifyScraper.java $url)
             YT_URLS=( $YT_URL_TEMP )
 
             # Download each song as MP3
-            for video_id in "${YT_URLS[@]}";
-                do echo "$video_id";
-            done
+             for video_id in "${YT_URLS[@]}";
+             do
+                #printf "$video_id"
+                node youtube_mp3.js "$HOME/Music" $video_id
+             done
         fi
 
         argNUM=$((argNUM+1)) 
 done
 
+# Help event (if executed without any arguments)
 if [ $argNUM == 1 ]
 then
     printf "\n${RED}(HELP) Here is a list of commands you can use:${NC}"
     printf "\n--help | -h (For a list of commands)"
-    printf "\n--playlist playlistURL\n"
+    printf "\n--playlist playlistURL\n\n"
 fi
+exit
